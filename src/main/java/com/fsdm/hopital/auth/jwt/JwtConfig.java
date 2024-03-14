effead9817f697f316d33fde17e262fd75ebe57f
Dabaghi-Anass <anass.dabaghi123@gmail.com>
2024-03-14 19:56:00 +0000

    disabled security filter chain (dev only)

---
+++ src/main/java/com/fsdm/hopital/auth/jwt/JwtConfig.java
@@ -0,0 +1,10 @@
+package com.fsdm.hopital.auth.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtConfig {
+}
---
+++ src/main/java/com/fsdm/hopital/filters/JwtFilter.java
@@ -0,0 +1,10 @@
+package com.fsdm.hopital.filters;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter {
+}
